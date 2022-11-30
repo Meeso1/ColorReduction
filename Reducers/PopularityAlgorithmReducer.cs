8732c383592201d80f6beee065414b84792938e8
Meeso1 <kubaknyspel@gmail.com>
2022-11-30 21:02:25 +0100

    Optimized popularity algorithm reducer to cache reduced colors in the tree.

--- Reducers/PopularityAlgorithmReducer.cs
+++ Reducers/PopularityAlgorithmReducer.cs
@@ -15,20 +15,10 @@ namespace ColorReduction.Reducers
             _numOfColors = numOfColors;
         }
 
-        private static Color FindClosest(Color input, IEnumerable<Color> palette)
-        {
-            double Distance(Color a, Color b)
-            {
-                return Math.Sqrt((a.R - b.R) * (a.R - b.R) + (a.G - b.G) * (a.G - b.G) + (a.B - b.B) * (a.B - b.B));
-            }
-
-            return palette.OrderBy(c => Distance(c, input)).First();
-        }
-
         public override Bitmap Reduce(Bitmap image)
         {
-            var tree = new ColorTree();
-            var palette = tree.GetPalette(image, _numOfColors);
+            var tree = new ColorTree(image, _numOfColors);
+            tree.InitPalette();
 
             var output = new Bitmap(image.Width, image.Height);
             using var bitmap = new FastBitmap(output);
@@ -37,7 +27,7 @@ namespace ColorReduction.Reducers
                 for (var x = 0; x < bitmap.Width; x++)
                 {
                     var color = image.GetPixel(x, y);
-                    var newColor = FindClosest(color, palette);
+                    var newColor = tree.ReduceColor(color);
                     output.SetPixel(x, y, newColor);
                 }
 
@@ -46,22 +36,51 @@ namespace ColorReduction.Reducers
 
         private sealed class ColorTree
         {
+            private readonly Bitmap _bitmap;
             private readonly List<Node> _leaves = new List<Node>();
+            private readonly int _paletteSize;
             private readonly Node _root;
+            private List<Color>? _palette;
 
-            public ColorTree()
+            public ColorTree(Bitmap bitmap, int paletteSize)
             {
                 _root = new Node(0, null, _leaves);
+                _bitmap = bitmap;
+                _paletteSize = paletteSize;
             }
 
-            public List<Color> GetPalette(Bitmap bitmap, int number)
+            public void InitPalette()
             {
-                for (var y = 0; y < bitmap.Height; y++)
-                    for (var x = 0; x < bitmap.Width; x++)
-                        _root.Add(bitmap.GetPixel(x, y), _leaves);
+                if (_palette is { }) return;
+
+                for (var y = 0; y < _bitmap.Height; y++)
+                    for (var x = 0; x < _bitmap.Width; x++)
+                        _root.Add(_bitmap.GetPixel(x, y), _leaves);
 
                 _leaves.Sort((a, b) => a.Count - b.Count);
-                return _leaves.Take(number).Select(l => l.Color!.Value).ToList();
+                _palette = _leaves.Take(_paletteSize).Select(l => l.Color!.Value).ToList();
+                foreach (var leaf in _leaves.Skip(_paletteSize)) leaf.Clear();
+            }
+
+            public Color ReduceColor(Color color)
+            {
+                var leaf = _root.FindLeaf(color);
+                if (leaf.Color is { }) return leaf.Color.Value;
+                var reduced = FindClosest(color);
+                leaf.Set(reduced);
+                return color;
+            }
+
+            private Color FindClosest(Color input)
+            {
+                if (_palette is null) throw new InvalidOperationException("Palette of color tree was uninitialized");
+
+                double Distance(Color a, Color b)
+                {
+                    return (a.R - b.R) * (a.R - b.R) + (a.G - b.G) * (a.G - b.G) + (a.B - b.B) * (a.B - b.B);
+                }
+
+                return _palette.OrderBy(c => Distance(c, input)).First();
             }
 
             private sealed class Node
@@ -86,7 +105,7 @@ namespace ColorReduction.Reducers
                 }
 
                 public int Count { get; private set; }
-                public Color? Color { get; }
+                public Color? Color { get; private set; }
 
                 public void Add(Color color, ICollection<Node> leaves)
                 {
@@ -103,6 +122,29 @@ namespace ColorReduction.Reducers
 
                     _children[index] ??= new Node(_depth + 1, color, leaves);
                     _children[index]!.Add(color, leaves);
+                }
+
+                public void Clear()
+                {
+                    Color = null;
+                }
+
+                public void Set(Color color)
+                {
+                    Color = color;
+                }
+
+                public Node FindLeaf(Color color)
+                {
+                    if (_depth == 8) return this;
+
+                    var index = 0;
+                    if ((color.R & (128 >> _depth)) > 0) index += 4;
+                    if ((color.G & (128 >> _depth)) > 0) index += 2;
+                    if ((color.B & (128 >> _depth)) > 0) index += 1;
+
+                    return _children[index]?.FindLeaf(color) ??
+                           throw new InvalidOperationException("Attempted to find unregistered color");
                 }
             }
         }
